b06d9c64645248efe93ea6cdf175a065e4b22eb2
Okiror samuel vinald <okiror1vinald@gmail.com>
2023-11-11 23:46:35 +0300

    migraions

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+    ('stationary', 'stationary'),
+    ('electronics', 'electronics'),
+    ('food', 'food'),
+)
+
+
+class Quantity(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
